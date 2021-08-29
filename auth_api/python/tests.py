4fa8afde8c11138da44b6e004c359944eac39189
jamalpica <jmalpica@gmail.com>
2021-08-29 04:15:31 -0500

    Modify unit test. Add two draft Healthcheck test. (replace service.local with dns alias or pod/container ip) it will need QC

--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -1,5 +1,8 @@
 import unittest
+import requests
+import httpretty
 from methods import Token, Restricted
+
 
 
 class TestStringMethods(unittest.TestCase):
@@ -7,6 +10,18 @@ class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.convert = Token()
         self.validate = Restricted()
+
+
+    def test_hc01(self):
+        response = requests.get('http://service.local:8000/')
+        self.assertEqual(response.status_code, 200)
+
+    @httpretty.activate
+    def test_hc02(self):
+        httpretty.register_uri(httpretty.GET, "http://service.local:8000/_health", body="OK")
+        # use!
+        response = requests.get('http://service.local:8000/_health')
+        self.assertEqual(response.text, 'OK')
 
     def test_generate_token(self):
         self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
