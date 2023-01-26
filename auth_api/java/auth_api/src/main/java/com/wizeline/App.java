d7b43722802769563b2181f2f3471a5c7552662f
gdanielcastellanos <gerardo.castellanos@wizeline.com>
2023-01-26 15:31:11 -0600

    indentation changes

--- auth_api/java/auth_api/src/main/java/com/wizeline/App.java
+++ auth_api/java/auth_api/src/main/java/com/wizeline/App.java
@@ -8,35 +8,35 @@ import static spark.Spark.*;
 
 public class App {
 
-  private static final Logger log = LoggerFactory.getLogger(App.class);
+    private static final Logger log = LoggerFactory.getLogger(App.class);
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    log.info("Listening on: http://localhost:8000/");
+        log.info("Listening on: http://localhost:8000/");
 
-    port(8000);
-    get("/", App::routeRoot);
-    get("/_health", App::routeRoot);
-    post("/login", App::urlLogin, json());
-    get("/protected", App::protect, json());
-  }
+        port(8000);
+        get("/", App::routeRoot);
+        get("/_health", App::routeRoot);
+        post("/login", App::urlLogin, json());
+        get("/protected", App::protect, json());
+    }
 
-  public static Object routeRoot(spark.Request req, spark.Response res) throws Exception {
-    return "OK";
-  }
+    public static Object routeRoot(spark.Request req, spark.Response res) throws Exception {
+        return "OK";
+    }
 
-  public static Object urlLogin(spark.Request req, spark.Response res) throws Exception {
-    String username = req.queryParams("username");
-    String password = req.queryParams("password");
-    Response r = new Response(Methods.generateToken(username, password));
-    res.type("application/json");
-    return r;
-  }
+    public static Object urlLogin(spark.Request req, spark.Response res) throws Exception {
+        String username = req.queryParams("username");
+        String password = req.queryParams("password");
+        Response r = new Response(Methods.generateToken(username, password));
+        res.type("application/json");
+        return r;
+    }
 
-  public static Object protect(spark.Request req, spark.Response res) throws Exception {
-    String authorization = req.headers("Authorization");
-    Response r = new Response(Methods.accessData(authorization));
-    res.type("application/json");
-    return r;
-  }
+    public static Object protect(spark.Request req, spark.Response res) throws Exception {
+        String authorization = req.headers("Authorization");
+        Response r = new Response(Methods.accessData(authorization));
+        res.type("application/json");
+        return r;
+    }
 }
